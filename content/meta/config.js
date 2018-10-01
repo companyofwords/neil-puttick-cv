9964f4eedd1799a311b1181f8271b906a058261e
Neil Puttick <m@idst.org>
2018-10-01 07:28:35 +0100

    updated siteurl

--- content/meta/config.js
+++ content/meta/config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
   shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
   siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteUrl: "quizzical-darwin-8032ac.netlify.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
