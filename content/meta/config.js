7123d70d2e4c964f1a90e3870415b2e69a9e6762
Neil Puttick <m@idst.org>
2018-10-01 08:15:00 +0100

    config added protocol

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,32 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "quizzical-darwin-8032ac.netlify.com",
+  siteTitle: "Neil Puttick - Digital Creative Producer & Front-End Web Developer", // <title>
+  shortSiteTitle: "Neil Puttick - Digital Creative Producer & Front-End Web Developer", // <title> ending for posts and pages
+  siteDescription: "Neil Puttick - Digital Creative Producer & Front-End Web Developer.",
+  siteUrl: "https://quizzical-darwin-8032ac.netlify.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Neil Puttick",
+  authorTwitterAccount: "companyofwords",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Neil Puttick",
+  infoTitleNote: "Digital Creative Producer & Front-End Web Developer",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "Neil Puttick - Front-End Web Developer",
+  manifestShortName: "Neil Puttick", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "neil@wordsco.uk",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/companyofwords" },
+    { name: "twitter", url: "https://twitter.com/companyofwords" },
+    { name: "facebook", url: "http://facebook.com/nfputtick" }
   ]
 };
