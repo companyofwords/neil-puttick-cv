5a89dc17f5bb14cc5526ff359aa318b78732f4b4
Neil Puttick <m@idst.org>
2018-10-01 09:41:08 +0100

    remove post references

--- src/components/ActionsBar/CategoryFilter.js
+++ src/components/ActionsBar/CategoryFilter.js
@@ -83,7 +83,7 @@ class CategoryFilter extends React.Component {
                 <Paper>
                   <MenuList role="menu">
                     <MenuItem key="all" onClick={this.handleFiltering}>
-                      all posts
+                      Areas of Expertise
                     </MenuItem>
                     {categories.map(category => (
                       <MenuItem key={category} onClick={this.handleFiltering}>
